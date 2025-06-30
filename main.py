1f812e683616545f3d984060c900e3efe47858b0
BowenZ217 <bowzhu721@gmail.com>
2025-06-29 23:35:24 -0700

    refactor(project): migrate project to src layout

--- main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Main entry point for the Noval downloader application.
-"""
-
-from novel_downloader.cli.main import cli_main
-
-if __name__ == "__main__":
-    cli_main()
