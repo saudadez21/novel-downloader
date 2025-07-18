90f46666a7ce2b85ea63654e9e84f0c9bd762bb3
BowenZ217 <bowzhu721@gmail.com>
2025-07-18 11:34:10 -0700

    refactor(parser): flatten parser directories

--- src/novel_downloader/core/parsers/qianbi.py
+++ src/novel_downloader/core/parsers/qianbi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-novel_downloader.core.parsers.qianbi.main_parser
-------------------------------------------------
+novel_downloader.core.parsers.qianbi
+------------------------------------
 
 """
 
