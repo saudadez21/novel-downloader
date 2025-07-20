7089c7d7f922499ecc1602e36422965ec75552be
BowenZ217 <bowzhu721@gmail.com>
2025-07-20 00:29:40 -0700

    feat(search): implement QidianSearcher

--- src/novel_downloader/core/searchers/__init__.py
+++ src/novel_downloader/core/searchers/__init__.py
@@ -4,3 +4,11 @@ novel_downloader.core.searchers
 -------------------------------
 
 """
+
+__all__ = [
+    "search",
+    "QidianSearcher",
+]
+
+from .qidian import QidianSearcher
+from .registry import search
